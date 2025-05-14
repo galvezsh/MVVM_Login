f668e47aba4b39e3b4adf400f5637cf5f2b76dd0
Galvesh <galvezssr@gmail.com>
2025-05-14 13:39:47 +0200

    TextField working without MVVM

--- app/src/main/java/com/galvezsh/mvvm_login/ui/theme/login/ui/LoginScreen.kt
+++ app/src/main/java/com/galvezsh/mvvm_login/ui/theme/login/ui/LoginScreen.kt
@@ -12,12 +12,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,6 +35,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -90,15 +101,16 @@ fun HeaderImage( modifier: Modifier ) {
 
 @Composable
 fun EmailField() {
+    var email by remember { mutableStateOf("") }
+
     TextField(
-        value = "",
+        value = email,
         enabled = true,
-        onValueChange = {},
+        onValueChange = { email = it },
         modifier = Modifier.fillMaxWidth(),
         placeholder = { Text( text = "Email" ) },
         keyboardOptions = KeyboardOptions( keyboardType = KeyboardType.Email ),
         singleLine = true,
-        maxLines = 1,
         colors = TextFieldDefaults.colors(
             focusedTextColor = Color( 0xFF636262 ),
             unfocusedTextColor = Color( 0xFF636262 ),
@@ -112,15 +124,18 @@ fun EmailField() {
 
 @Composable
 fun PasswordField() {
+    var password by remember { mutableStateOf("") }
+    var visiblePassword by remember { mutableStateOf(false) }
+
     TextField(
-        value = "",
+        value = password,
         enabled = true,
-        onValueChange = {},
+        onValueChange = { password = it },
         modifier = Modifier.fillMaxWidth(),
+        visualTransformation = if (visiblePassword) VisualTransformation.None else PasswordVisualTransformation(),
         placeholder = { Text( text = "Password" ) },
         keyboardOptions = KeyboardOptions( keyboardType = KeyboardType.Password ),
         singleLine = true,
-        maxLines = 1,
         colors = TextFieldDefaults.colors(
             focusedTextColor = Color( 0xFF636262 ),
             unfocusedTextColor = Color( 0xFF636262 ),
@@ -128,7 +143,15 @@ fun PasswordField() {
             unfocusedContainerColor = Color( 0xFFDEDDDD ),
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent
-        )
+        ),
+        trailingIcon = {
+            val image = if (visiblePassword) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+            val description = if (visiblePassword) "Hide Password" else "Show Password"
+
+            IconButton( onClick = { visiblePassword = !visiblePassword } ) {
+                Icon( imageVector = image, contentDescription = description )
+            }
+        }
     )
 }
 
